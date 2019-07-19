6f70780b41a2bf1febc09ca1681a029a3f552d9c
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-07-19 13:37:43 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,25 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  # before_action :set_student, only: :show
 
    def index
     @students = Student.all
   end
 
- 
+  def show
+    find_student do |student|
+      render :show
+      
+    end
+  end
+
+  def find_student
+    @student = Student.find(params[:id])
+    
+  end
 
   
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+    # def set_student
+    #   @student = Student.find(params[:id])
+    # end
 end
