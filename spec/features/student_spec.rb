d0af41068fbe8f7d3630668368bd29b02cd40514
Amanda Treutler <amandathedev@gmail.com>
2019-07-19 15:36:52 -0500

    Done.

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -1,15 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'Route to view' do
-  it 'has an index page' do
+describe "Route to view" do
+  it "has an index page" do
     visit "/students"
 
     expect(page.status_code).to eq(200)
   end
 end
 
-describe 'Multiple students are shown' do
-  it 'on the index page' do
+describe "Multiple students are shown" do
+  it "on the index page" do
     Student.create!(first_name: "Daenerys", last_name: "Targaryen")
     Student.create!(first_name: "Lindsey", last_name: "Stirling")
 
@@ -19,22 +19,22 @@ describe 'Multiple students are shown' do
   end
 end
 
-describe 'Show page' do
+describe "Show page" do
   before do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
   end
 
-  it 'renders properly' do
+  it "renders properly" do
     visit "students/#{@student.id}"
     expect(page.status_code).to eq(200)
   end
 
-  it 'renders the first name in a h1 tag' do
+  it "renders the first name in a h1 tag" do
     visit "students/#{@student.id}"
     expect(page).to have_css("h1", text: "Daenerys")
   end
 
-  it 'renders the last name in a h1 tag' do
+  it "renders the last name in a h1 tag" do
     visit "students/#{@student.id}"
     expect(page).to have_css("h1", text: "Targaryen")
   end
