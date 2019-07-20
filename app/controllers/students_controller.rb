211b50ee01f8c79023c8b5e5977081ed140363f9
github username <github email address>
2019-07-19 23:15:06 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,12 @@
 class StudentsController < ApplicationController
-  def index
-    @students = Student.all
-  end
+  
+    def index
+        @students = Student.all
+    end
+
+    # Added def show method in order for specific student to be displayed. 
+    def show
+        @student = Student.find_by_id(params[:id])
+    end
+
 end
